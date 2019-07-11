61c5638f8f44f2ab84f4ba9ac35ea9f97f11e383
Lee Thuc <36508808+wathui99@users.noreply.github.com>
2019-07-11 16:08:12 +0700

    add code to nrf

--- stm8l151k6t6/nRF24/Transmit/V01/inc/nRF24.h
+++ stm8l151k6t6/nRF24/Transmit/V01/inc/nRF24.h
@@ -7,13 +7,50 @@
 #include "spi.h"
 #include "gpio.h"
 
+typedef enum {
+  nRF_MaskInterrupt_Disable = (uint8_t)0x00,
+  nRF_MaskInterrupt_MaxTries = (uint8_t)0x10,
+  nRF_MaskInterrupt_TX = (uint8_t)0x20,
+  nRF_MaskInterrupt_RX = (uint8_t)0x40
+} nRF_MaskInterrupt_TypeDef;
+typedef enum {
+  nRF_Mode_TX    = (uint8_t)0x00,
+  nRF_Mode_RX    = (uint8_t)0x01
+} nRF_Mode_TypeDef;
+typedef enum {
+  nRF_CRC_Disable = (uint8_t)0x00,
+  nRF_CRC_1Byte    = (uint8_t)0x08,
+  nRF_CRC_2Bytes    = (uint8_t)0x0C
+} nRF_CRC_TypeDef;
+typedef enum {
+  nRF_AA_Disable = (uint8_t)0x00,
+  nRF_AA_Pipe_0 = (uint8_t)0x01,
+  nRF_AA_Pipe_1 = (uint8_t)0x02,
+  nRF_AA_Pipe_2 = (uint8_t)0x04,
+  nRF_AA_Pipe_3 = (uint8_t)0x08,
+  nRF_AA_Pipe_4 = (uint8_t)0x10,
+  nRF_AA_Pipe_5 = (uint8_t)0x20
+} nRF_AA_TypeDef;
+typedef enum {
+  nRF_Pipe_Disable = (uint8_t)0x00,
+  nRF_EN_Pipe_0 = (uint8_t)0x01,
+  nRF_EN_Pipe_1 = (uint8_t)0x02,
+  nRF_EN_Pipe_2 = (uint8_t)0x04,
+  nRF_EN_Pipe_3 = (uint8_t)0x08,
+  nRF_EN_Pipe_4 = (uint8_t)0x10,
+  nRF_EN_Pipe_5 = (uint8_t)0x20
+} nRF_EN_Pipe_TypeDef;
+
+typedef enum {
+  nRF_Add_Width_3Bytes = (uint8_t)0x01,
+  nRF_Add_Width_4Bytes = (uint8_t)0x02,
+  nRF_Add_Width_5Bytes = (uint8_t)0x03,
+} nRF_Add_Width_TypeDef;
+
 #define CSS_GPIO GPIOB
 #define CSS_PIN 4
 #define CE_GPIO GPIOB
 #define CE_PIN 3
-
-#define CSS_HIGH() (sbi(GPIOB, 4))
-#define CSS_LOW() (cbi(GPIOB, 4))
 
 static uint8_t nRF_payloadLength;
 
@@ -21,5 +58,11 @@ void nRF_Set_Payload_Leng(uint8_t value);
 uint8_t nRF_Get_Status_Reg();
 void nRF_Write_One_Reg(uint8_t reg, uint8_t value);
 uint8_t nRF_Read_One_Reg(uint8_t reg);
+void nRF_Write_Multi_Regs(uint8_t reg, uint8_t *value, uint8_t len);
+uint8_t *nRF_Read_Multi_Regs(uint8_t reg, uint8_t *ret, uint8_t len);
 
+void nRF_Config(nRF_MaskInterrupt_TypeDef MaskInterrupt, nRF_CRC_TypeDef CRC, nRF_Mode_TypeDef Mode, \
+                nRF_AA_TypeDef AA, nRF_EN_Pipe_TypeDef EN_Pipe, nRF_Add_Width_TypeDef Add_Width, \
+                
+                  
 #endif /* __nRF24_H */
--- stm8l151k6t6/nRF24/Transmit/V01/src/nRF24.c
+++ stm8l151k6t6/nRF24/Transmit/V01/src/nRF24.c
@@ -7,10 +7,11 @@
 
 uint8_t nRF_payloadLength=0;
 
+/* set static payload for send or receive */
 void nRF_Set_Payload_Leng(uint8_t value) {
   nRF_payloadLength=value;
 }
-
+/* get status register */
 uint8_t nRF_Get_Status_Reg() {
   uint8_t ret=0;
   GPIO_Write_Bit(CSS_GPIO, CSS_PIN, OFF);
@@ -18,12 +19,33 @@ uint8_t nRF_Get_Status_Reg() {
   GPIO_Write_Bit(CSS_GPIO, CSS_PIN, ON);
   return ret;
 }
+/* Write just one byte into one register */
 void nRF_Write_One_Reg(uint8_t reg, uint8_t value) {
   GPIO_Write_Bit(CSS_GPIO, CSS_PIN, OFF);
   SPI_Write_One_Byte(W_REGISTER | (REGISTER_MASK & reg));
   SPI_Write_One_Byte(value);
   GPIO_Write_Bit(CSS_GPIO, CSS_PIN, ON);
 }
+/* Read just one byte from one register */
 uint8_t nRF_Read_One_Reg(uint8_t reg) {
   return SPI_Send_Sync_One(R_REGISTER | (reg & REGISTER_MASK));
+}
+/* Write multi bytes into registers (base_address = reg) */
+void nRF_Write_Multi_Regs(uint8_t reg, uint8_t *value, uint8_t len) {
+  GPIO_Write_Bit(CSS_GPIO, CSS_PIN, OFF);
+  SPI_Write_One_Byte(W_REGISTER | (REGISTER_MASK & reg));
+  for (int8_t i=0; i<len; i++) {
+    SPI_Write_One_Byte(value[i]);
+  }
+  GPIO_Write_Bit(CSS_GPIO, CSS_PIN, ON);
+}
+/* Read multi bytes from registers (base_address = reg) */
+uint8_t *nRF_Read_Multi_Regs(uint8_t reg, uint8_t *ret, uint8_t len) {
+  GPIO_Write_Bit(CSS_GPIO, CSS_PIN, OFF);
+  SPI_Write_One_Byte(R_REGISTER | (REGISTER_MASK & reg));
+  for (int8_t i=0; i<len; i++) {
+    ret[i]=SPI_Send_Sync_One(NOP);
+  }
+  GPIO_Write_Bit(CSS_GPIO, CSS_PIN, ON);
+  return ret;
 }
