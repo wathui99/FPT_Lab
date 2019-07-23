7adc0514ddaa12b866a51dec8b4dd933be9abd42
NguyenLuongDuyKhanh <16520584@gm.uit.edu.vn>
2019-07-23 09:44:55 +0700

    craft docs

--- rtos_lab/project/settings/rtos.Debug.cspy.bat
+++ rtos_lab/project/settings/rtos.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\ThucLT\project\FPT_Lab\rtos_lab\project\settings\rtos.Debug.general.xcl" --backend -f "D:\ThucLT\project\FPT_Lab\rtos_lab\project\settings\rtos.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\WorkingZone\RTOS\FPT_Lab\rtos_lab\project\settings\rtos.Debug.general.xcl" --backend -f "D:\WorkingZone\RTOS\FPT_Lab\rtos_lab\project\settings\rtos.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\ThucLT\project\FPT_Lab\rtos_lab\project\settings\rtos.Debug.general.xcl" "--debug_file=%~1" --backend -f "D:\ThucLT\project\FPT_Lab\rtos_lab\project\settings\rtos.Debug.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "D:\WorkingZone\RTOS\FPT_Lab\rtos_lab\project\settings\rtos.Debug.general.xcl" "--debug_file=%~1" --backend -f "D:\WorkingZone\RTOS\FPT_Lab\rtos_lab\project\settings\rtos.Debug.driver.xcl" 
 
 @echo off 
 :end
